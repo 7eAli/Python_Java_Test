881d5e315c69dc45a1250b6c0c70622746f36062
GSY <gle722@yandex.ru>
2023-08-17 14:12:34 +0300

    Добавил сортировку по дате. Добавил файл-заглушку в папку notes, чтобы она прошла на гит

--- Python_task/Notebook.py
+++ Python_task/Notebook.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from email.utils import parsedate_to_datetime
 from genericpath import isfile
+from operator import itemgetter
 import os
 
 def generalCommandList():
@@ -13,6 +15,14 @@ def noteCommandList():
 	print("Введите 3 для удаления заметки")
 	print("Введите 4 для вывода всех заметок")
 	print("Введите 5 для вывода нужной заметки")
+	print("Введите 6 для вывода заметок с учетом даты")
+	print("Введите stop для выхода из редактора")
+
+def dateSortCommandList():
+	print("Введите 1 для сортировки по дате создания по убыванию")
+	print("Введите 2 для сортировки по дате создания по возрастанию")
+	print("Введите 3 для сортировки по дате изменения по убыванию")
+	print("Введите 4 для сортировки по дате изменения по возрастанию")	
 	print("Введите stop для выхода из редактора")
 
 def fileManager():
@@ -38,22 +48,42 @@ def readFile(fileName):
 
 def parseFile(fileName):
 	with open(fileName, 'r') as file:
-		lines = file.readlines()
-		result = list()
-		for el in lines:
-			result.append(el)		
-	return result
+		lines = file.readlines()	
+	return lines
 
 def parseNote(note):
 	result = note.split("|")
 	return result
 
+def parseDate(date):
+	result = date.split("=")
+	return result
+
+def createListOfDicts(fileName):
+	notes = parseFile(fileName)
+	result = list()
+	for el in notes:
+		note = parseNote(el)
+		creation_date = parseDate(note[3])
+		change_date = parseDate(note[4])
+		dict_element = dict()
+		dict_element['ID'] = note[0]
+		dict_element['Заголовок'] = note[1]
+		dict_element['Текст'] = note[2]
+		dict_element[creation_date[0]] = datetime.strptime(creation_date[1], "%Y-%m-%d %H:%M:%S")
+		dict_element[change_date[0]] = datetime.strptime(change_date[1], "%Y-%m-%d %H:%M:%S") 
+		result.append(dict_element)
+	return result
+
 def newNote(fileName):
 	notes = parseFile(fileName)
 	date = datetime.today()
+	print("Введите заголовок заметки: ")
+	head = input()
 	print("Введите текст заметки: ")
-	date_string = date.strftime("%Y-%m-%d_%H:%M:%S")
-	notes.append(f"{len(notes) + 1} | {input()} | {date_string} | Даты изменений: |" + "\n")
+	body = input()
+	date_string = date.strftime("%Y-%m-%d %H:%M:%S")
+	notes.append(f"{len(notes) + 1}|{head}|{body}|Дата создания={date_string}|Дата последнего изменения={date_string}|" + "\n")
 	with open(fileName, 'w') as file:
 		for el in notes:			
 			file.writelines(el)		
@@ -65,11 +95,11 @@ def changeNote(fileName):
 	if note_number <= len(notes):
 		note = parseNote(notes[note_number - 1])
 		print("Введите измененную заметку: ")
-		note[1] = input()
+		note[2] = input()
 		date = datetime.today()
-		date_string = date.strftime("%Y-%m-%d_%H:%M:%S")
-		note[3] += f"{date_string} | \n"
-		notes[note_number - 1] = f"{note_number} | {note[1]} |{note[2]}|{note[3]}"
+		date_string = date.strftime("%Y-%m-%d %H:%M:%S")
+		note[4] = f" Дата последнего изменения={date_string}| \n"
+		notes[note_number - 1] = f"{note_number}|{note[1]}|{note[2]}|{note[3]}|{note[4]}"
 		with open(fileName, 'w') as file:
 			for el in notes:
 				file.writelines(el)
@@ -84,7 +114,7 @@ def deleteNote(fileName):
 		notes.pop(note_number - 1)		
 		for i in range(note_number - 1, len(notes)):
 			note = parseNote(notes[i])
-			notes[i] = f"{i + 1} |{note[1]}|{note[2]}|{note[3]}|{note[4]}"			
+			notes[i] = f"{i + 1}|{note[1]}|{note[2]}|{note[3]}|{note[4]}|"			
 		with open(fileName, 'w') as file:
 			for el in notes:
 				file.writelines(el)			
@@ -92,9 +122,13 @@ def deleteNote(fileName):
 		print("Номер заметки неправильный")
 
 def showNotes(fileName):
-	notes = parseFile(fileName)	
+	notes = createListOfDicts(fileName)
 	for el in notes:
-		print(el)		
+		print(f"{el['ID']}. Заголовок: {el['Заголовок']} | Текст: {el['Текст']} | Дата создания: {el['Дата создания']} | Дата последнего изменения: {el['Дата последнего изменения']}")		
+
+def altShowNotes(notes):
+	for el in notes:
+		print(f"{el['ID']}. Заголовок: {el['Заголовок']} | Текст: {el['Текст']} | Дата создания: {el['Дата создания']} | Дата последнего изменения: {el['Дата последнего изменения']}")		
 
 def showNote(fileName):
 	note_number = int(input("Введите номер заметки: "))
@@ -102,7 +136,32 @@ def showNote(fileName):
 	if note_number <= len(notes):
 		print(notes[note_number - 1])
 	else:
-		print("Номер заметки неправильный")
+		print("Номер заметки неправильный")		
+
+def showByDate(fileName):	
+	flag = True
+	while flag:
+		dateSortCommandList()
+		notes = createListOfDicts(fileName)
+		command = input()
+		if command == "1":
+			notes.sort(key=itemgetter('Дата создания'))
+			altShowNotes(notes)
+		elif command == "2":
+			notes.sort(reverse=True, key=itemgetter('Дата создания'))
+			altShowNotes(notes)
+		elif command == "3":
+			notes.sort(key=itemgetter('Дата последнего изменения'))
+			altShowNotes(notes)
+		elif command == "4":
+			notes.sort(reverse=True, key=itemgetter('Дата последнего изменения'))
+			altShowNotes(notes)
+		elif command == "stop":
+			flag = False
+		else:
+			print("Нет такой команды")
+		
+
 
 def commandsManager(fileName):		
 	flag = True
@@ -119,6 +178,8 @@ def commandsManager(fileName):
 			showNotes(fileName)
 		elif command == "5":
 			showNote(fileName)
+		elif command == "6":
+			showByDate(fileName)
 		elif command == "stop":
 			flag = False
 		else:
